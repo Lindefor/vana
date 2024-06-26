2814d42b558bd857bdbddd2eb757e8eb44b903f6
Martin Lindefors <lindefor@kth.se>
2024-06-26 12:06:16 +0200

    feat: initial page split and colorization

---
+++ src/lib/components/mainView/MainView.svelte
@@ -0,0 +1,21 @@
+<!-- 
+    This is the main view component of Vana
+-->
+
+<script lang="ts">
+    
+</script>
+
+<div class="mainView">
+    s
+</div>
+
+<style lang="scss">
+    .mainView {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        background-color: $darkModeLight;
+        margin-top: -29px;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,54 @@
 <script lang="ts">
-    import MiddleView from "$lib/components/MiddleView.svelte";
+    import LeftBar from "$lib/components/leftBar/LeftBar.svelte";
+    import HabitList from "$lib/components/habitList/HabitList.svelte";
+    import MiddleView from "$lib/components/mainView/MainView.svelte";
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    <title>Vana</title>
+    <meta name="noMetaName" content="Vana Demo App" />
+</svelte:head>
+
+<div class="wrapper">
+    <div class="topBar">Vana</div>
+    <div class="mainWrapper">
+        <LeftBar />
+        <HabitList />
+        <MiddleView />
+    </div>    
+</div>
+
+
+
+<style lang="scss">
+
+    .wrapper {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .topBar {
+        height: 30px;
+        width: 100%;
+        z-index: 1000;
+        -webkit-user-select: none;
+        user-select: none;
+        -webkit-app-region: drag;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: $fontColor;
+    }
+    
+    .mainWrapper {
+        display: flex;
+        flex-direction: row;
+    }
+
+    :global(body) {
+		margin: 0;
+		padding: 0;
+		font-family: 'Roboto', serif;
+		overflow: hidden;
+	}
+</style>
