15c98252a68249bbe14785d60a50ee61c914e249
Martin Lindefors <lindefor@kth.se>
2024-07-16 11:00:43 +0200

    add habitlist which is can be resized

--- src/lib/components/habitList/HabitList.svelte
+++ src/lib/components/habitList/HabitList.svelte
@@ -4,20 +4,65 @@
 
 <script lang="ts">
     import { HABIT_LIST_WIDTH } from "$lib/constants";
+	import { onDestroy, onMount } from "svelte";
+
+    let dragging: { startX: any; } | null = null;
+    let listWidth = HABIT_LIST_WIDTH-60;
+
+    function startDrag(e: MouseEvent) {
+        dragging = {startX: e.clientX};
+    }
+
+    function stopDrag() {
+        dragging = null;
+    }
+
+    function drag(e: MouseEvent) {
+        if (dragging) {
+            const dx = e.clientX - dragging.startX;
+            dragging.startX = e.clientX;
+            if (listWidth + dx >= HABIT_LIST_WIDTH-60) {
+                listWidth += dx;
+            }
+        }
+    }
+
+    onMount(() => {
+        window.addEventListener('mousemove', drag);
+        window.addEventListener('mouseup', stopDrag);
+        
+    })
+    // onDestroy(() => {
+    //         window.removeEventListener('mousemove', drag);
+    //         window.removeEventListener('mouseup', stopDrag);
+    // })  
+    
+    
 </script>
 
-<div class="habitList" style="width:{HABIT_LIST_WIDTH}px">
-    s
+<div class="list">
+    <div class="habitList" style="width:{listWidth}px;">s</div>
+    <div class="divider" role="button" tabindex="0" aria-label="Adjust Habit List size" on:mousedown={(e) => startDrag(e)}></div>
 </div>
 
 <style lang="scss">
-    .habitList {
+
+    .list {
         display: flex;
         flex-direction: row;
-        background-color: $darkModeDark;
-        border-left: 1px solid #494949;
-        border-right: 1px solid black;
         margin-top: -29px;
-        flex: 1;
+        border-left: 1px solid #494949;
+        // flex: 1;
+    }
+    .habitList {
+        background-color: $darkModeDark;
+        
+    }
+
+    .divider {
+        cursor: ew-resize;
+        width: 2px;
+        border-right: 1px solid black;
+        height: 100%;
     }
 </style>
