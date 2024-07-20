574ac5162becfd6f547243e5254cb49a4dd0982f
Martin Lindefors <lindefor@kth.se>
2024-07-20 12:36:11 +0200

    add: - ipc handling for communication between main process and render process - filesystem usage to store and read habits in userdata path - loading screen when app is starting

--- src/electron.cjs
+++ src/electron.cjs
@@ -1,28 +1,46 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 const path = require('path');
 const { app, BrowserWindow } = require('electron');
-const serve = require("electron-serve");
+const serve = require('electron-serve');
 const ws = require('electron-window-state');
+const { initIPC } = require('./electron-utility/ipc.cjs');
+const { createHabitDirectories } = require('./electron-utility/helpers.cjs');
 // const fs = require('fs');
 // try { require("electron-reloader")(module); } catch {}
 
 const port = process.env.PORT || 3000;
 const isDev = !app.isPackaged || process.env.NODE_ENV == 'development';
-const loadURL = serve({directory: "."});
+const loadURL = serve({ directory: '.' });
 
 /** @type {BrowserWindow} */
 let mainWindow;
+var splash;
 
 function loadDev(port) {
-    mainWindow.loadURL(`http://localhost:${port}`).catch(() => {
-        setTimeout(() => { loadDev(port); }, 200);
-    });
+	mainWindow.loadURL(`http://localhost:${port}`).catch(() => {
+		setTimeout(() => {
+			loadDev(port);
+		}, 200);
+	});
+}
+
+function loading() {
+	mainWindow.loadFile('loading.html');
+	splash = new BrowserWindow({
+		width: 500,
+		height: 300,
+		transparent: true,
+		frame: false,
+		alwaysOnTop: true
+	});
+	splash.loadFile('loading.html');
+	splash.center();
 }
 
 function createWindow() {
 	let mws = ws({
 		defaultWidth: 1000,
-		defaultHeight: 800,
+		defaultHeight: 800
 	});
 	// Create the browser window.
 	mainWindow = new BrowserWindow({
@@ -43,21 +61,23 @@ function createWindow() {
 		trafficLightPosition: { x: 15, y: 15 }
 	});
 
+	loading();
 	// Open the DevTools.
 	if (isDev) {
 		loadDev(port);
-		// mainWindow.webContents.openDevTools();
+		mainWindow.webContents.openDevTools();
 	} else {
 		// mainWindow.removeMenu();
-        loadURL(mainWindow);
+		loadURL(mainWindow);
 		// mainWindow.loadFile(path.join(__dirname, 'build', 'index.html'));
 	}
 
 	mainWindow.center();
 	mainWindow.once('ready-to-show', () => {
-		mainWindow.show();
+		// mainWindow.show();
 	});
-    mws.manage(mainWindow);
+	mws.manage(mainWindow);
+	initIPC(mainWindow);
 }
 
 // This method will be called when Electron has finished
@@ -65,6 +85,11 @@ function createWindow() {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
 	createWindow();
+	createHabitDirectories(mainWindow);
+	setTimeout(() => {
+		splash.close();
+		mainWindow.show();
+	}, 1700);
 	app.on('activate', function () {
 		// On macOS it's common to re-create a window in the app when the
 		// dock icon is clicked and there are no other windows open.
---
+++ src/preload.cjs
@@ -0,0 +1,28 @@
+const { contextBridge, ipcRenderer } = require('electron')
+const path = require('path');
+
+contextBridge.exposeInMainWorld('electron', {
+  getPathSeparator: () => path.sep,
+  getFilesRoot: () => ipcRenderer.invoke('get-files-root'),
+  readHabits: () => { //readFiles
+    ipcRenderer.send('load-habits'); //load-files
+  },
+  readFromPath: (pathToDir, file) => {
+    ipcRenderer.send('load-from-path', pathToDir, file);
+  },
+  copyFromPath: (fromPath, toPath) => {
+    ipcRenderer.send('copy-from-path', fromPath, toPath);
+  },
+  onLoadedHabits: (func) => {
+    ipcRenderer.on('habits-loaded', (_, data) => func(data));
+  },
+  onLoadedHabit: (func) => { //receiveLoadedFile
+    ipcRenderer.on('habit-loaded', (_, data) => func(data)); //file-loaded
+  },
+  habitSystemRemove: (func) => { //fileSystemRemove
+    ipcRenderer.on('hs-removed', (_, fullPath) => func(fullPath));
+  },
+  directoryChange: (func) => {
+    ipcRenderer.on('dir-changed', (_, dir, filename) => func(dir, filename));
+  },
+})
