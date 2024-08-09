968265f17ba87b7659e08ecb07462ca985cdaff1
Martin Lindefors <lindefor@kth.se>
2024-08-09 11:38:15 +0200

    add habit store communication with habit file

--- src/lib/stores/habits.ts
+++ src/lib/stores/habits.ts
@@ -11,6 +11,18 @@ function helper(habits: HabitDir) {
 
 }
 
+function getSubdir(dirName: String, habitDir: HabitDir) {
+	for (const subDir of habitDir.subDirs) {
+		if (subDir.name === dirName) {
+			return subDir;
+		}
+	}
+	
+	for (const subDir of habitDir.subDirs) {
+		return getSubdir(dirName, subDir);
+	}
+}
+
 const buildHabitStore = () => {
 	const startState: HabitDir = {
 		id: 0,
@@ -29,16 +41,58 @@ const buildHabitStore = () => {
 				return habitSystem;
 			});
 		},
+		addHabit(newHabit: Habit, category: string) {
+			update((habitSystem) => {
+				if (category === "No category") {
+					habitSystem.habits.push(newHabit);
+					window.electron.updateHabits(habitSystem, habitSystem.id)
+				} else {
+					let habitDir = getSubdir(category, habitSystem);
+					habitDir?.habits.push(newHabit);
+					window.electron.updateHabits(habitDir, String(habitDir.id));
+				}
+				return habitSystem;
+			})
+		},
+		updateHabit(updatedHabit: Habit, category: string) {
+			
+			update((habitSystem) => {
+				if (category === 'No Category') {
+					for (let i = 0; i < habitSystem.habits.length; i++) {
+						if (habitSystem.habits[i].id === updatedHabit.id) {
+							habitSystem.habits[i] = updatedHabit;
+							break;
+						}
+					}
+					window.electron.updateHabits(habitSystem, String(habitSystem.id));
+				} else {
+					let habitDir = getSubdir(category, habitSystem);
+					if (habitDir) {
+						for (let i = 0; i < habitDir.habits.length; i++) {
+							if (habitDir.habits[i].id === updatedHabit.id) {
+								habitDir.habits[i] = updatedHabit;
+								break;
+							}
+						}
+					}
+					window.electron.updateHabits(habitDir, String(habitDir?.id))
+				}
+				return habitSystem;
+			})	
+		},
 		logAllCompleted() {
 			update((habits) => {
 				helper(habits);
 				return habits
 			})
 		},
-		toggleHabitCompleted(habit: Habit) {
+		toggleHabitCompleted(habit: Habit, category: string) {
+			console.log(category);
+			
 			habit.completed = !habit.completed;
 			update((habits) => {
 				habits = {...habits};
+				window.electron.updateHabits(habits, String(habits.id))
 				return habits;
 			})
 		}
