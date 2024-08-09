f6cadccaa8fe162216c9e9aa3d7e654e34aae60e
Martin Lindefors <lindefor@kth.se>
2024-08-09 11:26:30 +0200

    add hash function to create unique id for new habits

--- src/lib/types/habits.ts
+++ src/lib/types/habits.ts
@@ -1,9 +1,32 @@
+
+
+
 export interface Habit {
-	id: number;
+	id: string;
 	name: string;
 	completed: boolean;
 	description: string;
 	deadline: string; 
+}
+
+export class Habit implements Habit {
+	constructor(name: string, completed: boolean, description: string, deadline: string) {
+		this.generateId(name, completed, description, deadline);
+		this.name = name;
+		this.completed = completed;
+		this.description = description;
+		this.deadline = deadline;
+		this.initializeId();
+    }
+
+    private async initializeId() {
+        this.id = await this.generateId(this.name, this.completed, this.description, this.deadline);
+    }
+
+    private async generateId(name: string, completed: boolean, description: string, deadline: string): Promise<string> {
+		const hash = await window.electron.generateHash(name, completed, description, deadline);
+        return hash;
+    }
 }
 
 export interface HabitDir {
