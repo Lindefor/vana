6580927901e3c068a7cd08430a9a4d0755b8215b
Martin Lindefors <lindefor@kth.se>
2024-09-21 17:16:36 +0200

    test: add first test and make sure tests execute as expected

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,10 @@
 import { expect, test } from '@playwright/test';
 
-test('home page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	await expect(page.locator('h1')).toBeVisible();
+// test('home page has expected h1', async ({ page }) => {
+// 	await page.goto('/');
+// 	await expect(page.locator('h1')).toBeVisible();
+// });
+
+test('1+1=2', async () => {
+	expect(1 + 1).toBe(2);
 });
